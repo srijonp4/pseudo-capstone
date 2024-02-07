53e0aa315e509edff44226bffa7ef66ff6d5f9cb
Srijon Paul <86311499+srijonp4@users.noreply.github.com>
2024-02-08 00:13:46 +0530

    type : "module" in package.json causes problem with the ts-node-dev and ts-node , so removing it.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
+import express from 'express';
+import { Request, Response } from 'express';
+import http from 'http';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import compression from 'compression';
+import cookieparser from 'cookie-parser';
+dotenv.config();
+const PORT = process.env.PORT ?? 5173;
+const app = express();
 
+/* middlewares */
+app.use(cookieparser('6*7d-dN5x2V4'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+const server = http.createServer(app);
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    msg: 'hello world',
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
